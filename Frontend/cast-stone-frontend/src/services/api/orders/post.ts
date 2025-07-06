99ec483c4c59145041008c03e8147bad19d49653
umerfarooqlaghari <mumerfarooqlaghari@gmail.com>
2025-07-07 00:34:57 +0500

    post disable

--- Frontend/cast-stone-frontend/src/services/api/orders/post.ts
+++ Frontend/cast-stone-frontend/src/services/api/orders/post.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { BaseService, ServiceUtils } from '../../config/baseService';
 import { ApiEndpoints } from '../../config/apiConfig';
 import { Order, CreateOrderRequest, CreateOrderItemRequest } from '../../types/entities';
