ffe8514a2340f7fd5cacc98a26460baa805684b4
umerfarooqlaghari <mumerfarooqlaghari@gmail.com>
2025-07-14 14:31:28 +0500

    Eslint issues fixed

--- Frontend/cast-stone-frontend/src/services/api/productDetails/get.ts
+++ Frontend/cast-stone-frontend/src/services/api/productDetails/get.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { BaseService } from '../../config/baseService';
 import { ApiEndpoints } from '../../config/apiConfig';
 import { ProductDetails } from '../../types/entities';
